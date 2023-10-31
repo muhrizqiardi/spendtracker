d19974166a50b04701249d5fcd8acb4a0f42c6a9
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-10-31 21:12:01 +0700

    feat: add struct tags for validation

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,11 +1,13 @@
 package dto
 
 type RegisterUserDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"email"`
+	FullName string `json:"fullName" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
 }
 
 type UpdateUserDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	FullName string `json:"fullName" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
 }
