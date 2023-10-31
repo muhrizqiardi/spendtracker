80f12cf4bda11e79df40e8ff70c94273ec480f1a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-10-31 21:12:09 +0700

    feat: complete user service
    Refs: #12

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/muhrizqiardi/spendtracker/internal/database/model"
 	"github.com/muhrizqiardi/spendtracker/internal/dto"
 	"github.com/muhrizqiardi/spendtracker/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
@@ -23,8 +25,15 @@ func NewUserService(ur repository.UserRepository) *userService {
 }
 
 func (us *userService) Register(payload dto.RegisterUserDTO) (model.User, error) {
-	// TODO: add validation
-	user, err := us.ur.Insert(payload.Email, payload.Password)
+	validate := validator.New()
+	if err := validate.Struct(payload); err != nil {
+		return model.User{}, err
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return model.User{}, err
+	}
+	user, err := us.ur.Insert(payload.Email, payload.FullName, string(hashedPassword))
 	if err != nil {
 		return model.User{}, err
 	}
@@ -33,15 +42,41 @@ func (us *userService) Register(payload dto.RegisterUserDTO) (model.User, error)
 }
 
 func (us *userService) GetOneByID(id int) (model.User, error) {
-	return model.User{}, nil
+	user, err := us.ur.GetOneByID(id)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
 }
 
 func (us *userService) GetOneByEmail(email string) (model.User, error) {
-	return model.User{}, nil
+	if err := validator.New().Var(email, "required,email"); err != nil {
+		return model.User{}, err
+	}
+	user, err := us.ur.GetOneByEmail(email)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
 }
 
 func (us *userService) UpdateOneByID(id int, payload dto.UpdateUserDTO) (model.User, error) {
-	return model.User{}, nil
+	validate := validator.New()
+	if err := validate.Struct(payload); err != nil {
+		return model.User{}, err
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return model.User{}, err
+	}
+	user, err := us.ur.UpdateOneByID(id, payload.Email, payload.FullName, string(hashedPassword))
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
 }
 
 func (us *userService) DeleteOneByID(id int) (model.User, error) {
