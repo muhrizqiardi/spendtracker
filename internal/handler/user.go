7161f66b6e13ea2f8f065e53aefcf88e02bcd15e
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-11-01 02:19:59 +0700

    feat: implement `GetOneByID` and `UpdateOneByID` methods

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/muhrizqiardi/spendtracker/internal/database/model"
 	"github.com/muhrizqiardi/spendtracker/internal/dto"
 	"github.com/muhrizqiardi/spendtracker/internal/service"
 	"github.com/muhrizqiardi/spendtracker/internal/util"
@@ -44,14 +47,66 @@ func (uh *userHandler) Register(c echo.Context) error {
 
 	return c.JSON(
 		http.StatusCreated,
-		util.CreateBaseResponse[any](true, "User Created", user),
+		util.CreateBaseResponse[any](true, "User found", user),
 	)
 }
 
 func (uh *userHandler) GetOneByID(c echo.Context) error {
-	return nil
+	userID, err := strconv.Atoi(c.Param("userID"))
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(
+			http.StatusNotFound,
+			util.CreateBaseResponse[any](false, "Not Found", nil),
+		)
+	}
+
+	user, err := uh.us.GetOneByID(userID)
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(
+			http.StatusNotFound,
+			util.CreateBaseResponse[any](false, "Not Found", nil),
+		)
+	}
+
+	return c.JSON(
+		http.StatusOK,
+		util.CreateBaseResponse[model.User](true, "User found", user),
+	)
 }
 
 func (uh *userHandler) UpdateOneByID(c echo.Context) error {
-	return nil
+	userID, err := strconv.Atoi(c.Param("userID"))
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(
+			http.StatusNotFound,
+			util.CreateBaseResponse[any](false, "Not Found", nil),
+		)
+	}
+
+	var payload dto.UpdateUserDTO
+	if err := c.Bind(&payload); err != nil {
+		fmt.Println("bebek")
+		c.Logger().Error(err)
+		return c.JSON(
+			http.StatusBadRequest,
+			util.CreateBaseResponse[any](false, "Bad Request", nil),
+		)
+	}
+
+	user, err := uh.us.UpdateOneByID(userID, payload)
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(
+			http.StatusInternalServerError,
+			util.CreateBaseResponse[any](false, "Internal Server Error", nil),
+		)
+	}
+
+	return c.JSON(
+		http.StatusOK,
+		util.CreateBaseResponse[model.User](true, "User updated", user),
+	)
 }
